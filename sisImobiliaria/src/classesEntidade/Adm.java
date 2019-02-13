d5f7f4ac84ceca8ce10c1d28c4263679284fc9c2
welerson <welerson.a.melo@gmail.com>
2019-02-13 00:32:27 -0300

    Criando classes TipoImovel, Situacao Contrato, Adm e Endereco

---
+++ sisImobiliaria/src/classesEntidade/Adm.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classesEntidade;
+
+/**
+ *
+ * @author welerson
+ */
+public class Adm {
+    
+    private String login;
+    private String senha;
+    private String nomeCompleto;
+    private String telefone;
+    private String email;
+    
+    //Construtores
+    
+    public Adm(String login, String senha, String nomeCompleto, String telefone, String email) {
+        this.login = login;
+        this.senha = senha;
+        this.nomeCompleto = nomeCompleto;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    public Adm() {
+        
+    }
+    
+    //Gets e sets
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ sisImobiliaria/src/classesEntidade/Endereco.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classesEntidade;
+
+/**
+ *
+ * @author welerson
+ */
+public class Endereco {
+    
+    private String CEP;
+    private String UF;
+    private String cidade;
+    private String bairro;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    
+    //Construtores
+
+    public Endereco(String CEP, String UF, String cidade, String bairro, String logradouro, String numero, String complemento) {
+        this.CEP = CEP;
+        this.UF = UF;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+    
+    public Endereco(){
+        
+    }
+    
+    //Gets e Sets
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+    
+    
+}
